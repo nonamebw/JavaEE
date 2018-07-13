c837004300db43b4589f62823daa2577deb96dd6
nonamebw <bowenpi@qq.com>
2018-07-13 19:37:54 +0800

    first commit

---
+++ spring_bean_1/service/UserService.java
@@ -0,0 +1,16 @@
+package cn.itcast.service;
+
+import cn.itcast.dao.UserDao;
+
+public class UserService {
+	
+	private UserDao userDao;
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	public void add() {
+		System.out.println("service..........");
+		userDao.add();
+	}
+}
